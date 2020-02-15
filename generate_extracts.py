081dc4a3056447a6ed41830d6aa63831353a318e
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2020-02-15 16:02:49 +0100

    rewrite to make it use a recursive aproach

--- generate_extracts.py
+++ generate_extracts.py
@@ -5,75 +5,93 @@ import os
 import time
 from concurrent.futures.thread import ThreadPoolExecutor
 from pathlib import Path
+from typing import List
 
 import mercantile
 
+# Todo: Make configurable
+path = "extracts"
+executor = ThreadPoolExecutor(max_workers=8)
+generated_tiles_file = Path('generated_tiles.json')
+start_zoom = 0
+end_zoom = 10
 
-def process_asyc(command, tile):
+smallest_tiles = []  # here we save the smallest generated tiles for each region
+all_generated_tiles = []
+futures = {}
+
+
+def process_asyc(tile: List[int], parent_tile: List[int]):
+    tile_path = get_tile_path(tile)
+    parent_path = get_tile_path(parent_tile)
+    box = mercantile.bounds(tile)
+    command = "osmconvert -b={},{},{},{} -o={} --complete-ways --complex-ways {}".format(
+        box.west,
+        box.south,
+        box.east,
+        box.north,
+        tile_path.as_posix(),
+        parent_path.as_posix())
     print("Start Processing: {}".format(tile))
     start = time.time()
     os.system(command)
     print("Finish Processing: {} in {:.0f} seconds".format(tile, time.time() - start))
-    return 0
+    enqueue_children(tile)
 
 
-# Todo: Make configurable
-path = "extracts"
-executor = ThreadPoolExecutor(max_workers=2)
-generated_tiles_file = Path('generated_tiles.json')
+def get_tile_path(tile: List[int]) -> Path:
+    return Path("{}/{}_{}_{}.pbf".format(path, tile[2], tile[0], tile[1]))
 
-smallest_tiles = []  # here we save the smallest generated tiles for each region
-all_generated_tiles = []
 
-for i in range(1, 10):
-    for x in range(0, (2 ** i)):
-        futures = {}
-        for y in range(0, (2 ** i)):
-            tile = [x, y, i]
-            tile_name = "{}/{}/{}".format(i, x, y)
-            box = mercantile.bounds(tile)
-            parent = mercantile.parent(tile)
-            parent_file = Path("{}/{}_{}_{}.pbf".format(
-                path,
-                parent[2],
-                parent[0],
-                parent[1]))
-            tile_path = Path("{}/{}_{}_{}.pbf".format(
-                path,
-                tile[2],
-                tile[0],
-                tile[1]))
-            if not parent_file.exists():
-                # parent was not build
-                continue
-            if parent_file.stat().st_size < 1.5 * 10 ** 9:
-                print("Do not build {} because parent is small".format(tile_name))
-                if parent_file.as_posix() not in smallest_tiles:
-                    smallest_tiles.append(parent_file.as_posix())
-                continue
-            if tile_path.exists() and parent_file.stat().st_mtime < tile_path.stat().st_mtime:
-                print("Do not build {} because parent is older".format(tile_name))
-                continue
-            all_generated_tiles.append(tile_path.as_posix())
-            com = "osmconvert -b={},{},{},{} -o={} --complete-ways --complex-ways {}".format(
-                box.west,
-                box.south,
-                box.east,
-                box.north,
-                tile_path.as_posix(),
-                parent_file.as_posix())
-            futures[executor.submit(process_asyc, com, tile)] = tile_name
-        for future in concurrent.futures.as_completed(futures):
-            orig_tile_name = futures[future]
-            try:
-                data = future.result()
-            except Exception as exc:
-                print('%r generated an exception: %s' % (orig_tile_name, exc))
-            else:
-                # calculation ok
-                pass
+def enqueue_children(tile):
+    if tile[2] >= end_zoom:
+        return
+    tile_path = get_tile_path(tile)
+    if tile_path.stat().st_size < 1.5 * 10 ** 9:
+        print("Do not build children of {} because it is small".format(tile))
+        smallest_tiles.append(tile_path.as_posix())
+        return
+    children = mercantile.children(tile)
+    for child in children:
+        child_path = get_tile_path(child)
+        if child_path.exists() and tile_path.stat().st_mtime < child_path.stat().st_mtime:
+            print("Do not build {} because parent is older".format(child))
+        else:
+            print("Enqueue {}".format(child))
+            futures[executor.submit(process_asyc, child, tile)] = child
 
-print(smallest_tiles)
-with open(generated_tiles_file, 'w') as fp:
-    json.dump({'all': all_generated_tiles, 'smallest': smallest_tiles}, fp)
-print("Finished")
+
+def enqueue_initial():
+    for x in range(0, (2 ** start_zoom)):
+        for y in range(0, (2 ** start_zoom)):
+            tile = [x, y, start_zoom]
+            if get_tile_path(tile).exists():
+                enqueue_children(tile)
+
+
+def check_finished():
+    for future in concurrent.futures.as_completed(futures.copy()):
+        orig_tile_name = futures[future]
+        try:
+            data = future.result()
+        except Exception as exc:
+            print('%r generated an exception: %s' % (orig_tile_name, exc))
+        else:
+            # calculation ok
+            pass
+
+
+if __name__ == '__main__':
+    enqueue_initial()
+    try:
+        while futures:
+            print("Queue length: {}".format(len(futures)))
+            time.sleep(600)
+            check_finished()
+    except KeyboardInterrupt:
+        executor.shutdown()
+        raise
+    print(smallest_tiles)
+    with open(generated_tiles_file, 'w') as fp:
+        json.dump({'all': all_generated_tiles, 'smallest': smallest_tiles}, fp)
+    print("Finished")
