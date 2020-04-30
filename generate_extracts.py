e4a4761a4cf6a2358865900888cc3eee73e314b7
Finn-Thorben Sell <dev@finn-thorben.me>
2020-04-30 12:20:05 +0200

    use threadpool for parallel work

--- generate_extracts.py
+++ generate_extracts.py
@@ -3,7 +3,10 @@ import os
 import argparse
 import subprocess
 import mercantile
+import tqdm
+from multiprocessing.pool import Pool, ThreadPool
 from pathlib import Path
+from tqdm import tqdm
 
 
 def parse_args() -> argparse.Namespace:
@@ -32,55 +35,66 @@ def parse_args() -> argparse.Namespace:
 
 
 def download_planet_dump(url: str, working_dir: Path):
-    print(f"Downloading {url}")
+    print(f'Downloading {url}')
     subprocess.run(['wget', '-N', '-nv', '--show-progress', url], check=True, cwd=str(working_dir))
+
     file_name = url.rsplit('/', 1)[-1]
-    (working_dir / file_name).link_to(str(working_dir / 'source.osm.pbf'))
+    subprocess.run(['ln', '-sf', (working_dir / file_name).absolute(), (working_dir / 'source.osm.pbf').absolute()])
 
 
-def process_asyc(command):
-    com2 = "srun {}"
-    os.system(com2)
+def _process_tile(working_dir: Path, out_dir: Path, target_size: int, tile: mercantile.Tile):
+    box = mercantile.bounds(tile)
+    parent = mercantile.parent(tile)
+
+    parent_file = working_dir / f'{parent[2]}_{parent[0]}_{parent[1]}.pbf'
+    target_file = working_dir / f'{tile[2]}_{tile[0]}_{tile[1]}.pbf'
+
+    if not parent_file.exists():
+        print(f'not generating {tile} because parent does not exist')
+        return
+
+    if parent_file.stat().st_size < target_size:
+        print(f'not generating {tile} because parent has reached target size')
+        return
+
+    if not target_file.exists() or parent_file.stat().st_mtime > target_file.stat().st_mtime:
+        # only build file if it does not exist
+        # or parent file has been modified since target was last generated
+        print(f'generating {tile}')
+        cmd = [
+            'osmconvert',
+            f'-b={box.west},{box.south},{box.east},{box.north}',
+            f'-o={target_file.absolute()}',
+            '--complete-ways',
+            '--complex-ways',
+            '--out-pbf',
+            str(parent_file.absolute())
+        ]
+        subprocess.run(cmd, cwd=str(parent_file.parent), check=True)
+    else:
+        print(f'{tile} already exists and is current. skipping')
+
+    if target_file.stat().st_size < target_size:
+        print(f'{tile} has reached target size')
+        os.rename(str(target_file), str(out_dir / f'{tile[2]}_{tile[0]}_{tile[1]}.pbf'))
 
 
 def extract(working_dir: Path, out_dir: Path, target_size: int):
-    (working_dir / 'source.osm.pbf').link_to(str(working_dir / '0_0_0.pbf'))
-    tiles = []
+    print('Extracting tiles')
 
-    for z in range(1, 10):
-        for x in range(0, (2**z)):
-            for y in range(0, (2**z)):
-                tile = mercantile.Tile(x, y, z)
-                box = mercantile.bounds(tile)
-                parent = mercantile.parent(tile)
+    subprocess.run(['ln', '-sf', (working_dir / 'source.osm.pbf').absolute(), (working_dir / '0_0_0.pbf').absolute()])
 
-                parent_file = working_dir / f'{parent[2]}_{parent[0]}_{parent[1]}.pbf'
-                target_file = working_dir / f'{tile[2]}_{tile[0]}_{tile[1]}.pbf'
+    with tqdm(total=sum([2**(2*i) for i in range(1, 10)]), disable=True) as pbar:
+        for z in range(1, 10):
+            with ThreadPool(processes=max(os.cpu_count() - 2, 1)) as pool:
+                for x in range(0, (2**z)):
+                    for y in range(0, (2**z)):
+                        pbar.update(1)
 
-                if not parent_file.exists() or parent_file.stat().st_size < target_size:
-                    # parent was not build or has reached target size
-                    continue
-
-                if not target_file.exists() or parent_file.stat().st_mtime > target_file.stat().st_mtime:
-                    # only build file if it does not exist
-                    # or parent file has been modified since target was last generated
-                    cmd = [
-                        'osmconvert',
-                        f'-b={box.west},{box.south},{box.east},{box.north}',
-                        f'-o={target_file}',
-                        '--complete-ways',
-                        '--complex-ways',
-                        '--out-pbf',
-                        str(parent_file)
-                    ]
-                    subprocess.run(cmd, cwd=str(working_dir))
-
-                if target_file.stat().st_size < target_size:
-                    print(f'{tile} has reached target size')
-                    tiles.append(str(target_file))
-                    os.rename(str(target_file), str(out_dir / f'{tile[2]}_{tile[0]}_{tile[1]}.pbf'))
-
-    print(tiles)
+                        tile = mercantile.Tile(x, y, z)
+                        pool.apply_async(_process_tile, [working_dir, out_dir, target_size, tile])
+                pool.close()
+                pool.join()
 
 
 if __name__ == '__main__':
