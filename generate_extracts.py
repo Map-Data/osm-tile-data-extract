750fd9c286b4dbf85fe701a565a85f982513d659
Finn-Thorben Sell <dev@finn-thorben.me>
2020-05-07 13:07:42 +0200

    improve multiprocessing
    now not only each zoom level gets computed in parallel but instead each
    tile parallizes itself until the target size or maximum zoom level has
    been reached

--- generate_extracts.py
+++ generate_extracts.py
@@ -2,97 +2,146 @@
 import os
 import argparse
 import subprocess
+import time
+
 import mercantile
-from multiprocessing.pool import ThreadPool
+from multiprocessing import Lock
+from concurrent.futures import ThreadPoolExecutor, Future
 from pathlib import Path
 
 
-def parse_args() -> argparse.Namespace:
-    def directory_type(raw: str):
-        p = Path(raw)
-        p.mkdir(exist_ok=True)
-        if not p.is_dir():
-            raise argparse.ArgumentTypeError(f'Path {raw} is not a directory')
-        return p
+class Program:
+    @staticmethod
+    def parse_args() -> argparse.Namespace:
+        def directory_type(raw: str):
+            p = Path(raw)
+            p.mkdir(exist_ok=True)
+            if not p.is_dir():
+                raise argparse.ArgumentTypeError(f'Path {raw} is not a directory')
+            return p
 
-    parser = argparse.ArgumentParser('generate_extracts',
-                                     description='Extract similarly sized files from the latest OpenStreetMap '
-                                                 'Planet dump.')
-    parser.add_argument('-p', '--planet-dump', dest='planet_dump',
-                        default='https://planet.openstreetmap.org/pbf/planet-latest.osm.pbf',
-                        help='Url of the source pbf file')
-    parser.add_argument('-w', '--working-dir', dest='working_dir', type=directory_type,
-                        default=os.path.join(os.path.dirname(__file__), 'tmp'),
-                        help='Working directory to which the planet dump gets downloaded and in which intermediate '
-                             'split files are stored.')
-    parser.add_argument('-o', '--output-dir', dest='output_dir', type=directory_type,
-                        default=os.path.join(os.path.dirname(__file__), 'out'))
-    parser.add_argument('-s', '--target-size', dest='target_size', default=1.5*10**9, type=int,
-                        help='Target size which the split files should have')
-    return parser.parse_args()
+        parser = argparse.ArgumentParser('generate_extracts',
+                                         description='Extract similarly sized files from the latest OpenStreetMap '
+                                                     'Planet dump.')
+        parser.add_argument('-p', '--planet-dump', dest='planet_dump',
+                            default='https://planet.openstreetmap.org/pbf/planet-latest.osm.pbf',
+                            help='Url of the source pbf file')
+        parser.add_argument('-w', '--working-dir', dest='working_dir', type=directory_type,
+                            default=os.path.join(os.path.dirname(__file__), 'tmp'),
+                            help='Working directory to which the planet dump gets downloaded and in which intermediate '
+                                 'split files are stored.')
+        parser.add_argument('-o', '--output-dir', dest='output_dir', type=directory_type,
+                            default=os.path.join(os.path.dirname(__file__), 'out'))
+        parser.add_argument('-s', '--target-size', dest='target_size', default=1.5 * 10 ** 9, type=int,
+                            help='Target size which the split files should have')
+        return parser.parse_args()
 
+    def __init__(self):
+        args = self.parse_args()
+        self.working_dir = args.working_dir
+        self.out_dir = args.output_dir
+        self.target_size = args.target_size
+        self.planet_dump_url = args.planet_dump
 
-def download_planet_dump(url: str, working_dir: Path):
-    print(f'Downloading {url}')
-    subprocess.run(['wget', '-N', '-nv', '--show-progress', url], check=True, cwd=str(working_dir))
+        self.running_futures = 0
+        self.lock_running_futures = Lock()
 
-    file_name = url.rsplit('/', 1)[-1]
-    subprocess.run(['ln', '-sf', (working_dir / file_name).absolute(), (working_dir / 'source.osm.pbf').absolute()])
+        self.executor = ThreadPoolExecutor(max_workers=(max(1, os.cpu_count() - 2)))
 
+    def run(self):
+        self.download_planet_dump()
+        print('Extracting tiles')
+        self.extract(mercantile.Tile(0, 0, 0))
 
-def _process_tile(working_dir: Path, out_dir: Path, target_size: int, tile: mercantile.Tile):
-    box = mercantile.bounds(tile)
-    parent = mercantile.parent(tile)
+        while self.running_futures > 0:
+            time.sleep(10)
+        self.executor.shutdown(False)
 
-    parent_file = working_dir / f'{parent[2]}_{parent[0]}_{parent[1]}.pbf'
-    target_file = working_dir / f'{tile[2]}_{tile[0]}_{tile[1]}.pbf'
+    def download_planet_dump(self):
+        print(f'Downloading {self.planet_dump_url}')
+        subprocess.run(['wget', '-N', '-nv', '--show-progress', self.planet_dump_url],
+                       check=True, cwd=str(self.working_dir))
 
-    if not parent_file.exists():
-        print(f'not generating {tile} because parent does not exist')
-        return
+        file_name = self.planet_dump_url.rsplit('/', 1)[-1]
+        subprocess.run(['ln', '-sf',
+                        (self.working_dir / file_name).absolute(),
+                        (self.working_dir / '0_0_0.pbf').absolute()
+                        ])
 
-    if parent_file.stat().st_size < target_size:
-        print(f'not generating {tile} because parent has reached target size')
-        return
+    def _generate_tile(self, tile: mercantile.Tile):
+        """
+        Generate a single target tile from its parent by running osmconvert.
 
-    if not target_file.exists() or parent_file.stat().st_mtime > target_file.stat().st_mtime:
-        # only build file if it does not exist
-        # or parent file has been modified since target was last generated
-        print(f'generating {tile}')
-        cmd = [
-            'osmconvert',
-            f'-b={box.west},{box.south},{box.east},{box.north}',
-            f'-o={target_file.absolute()}',
-            '--complete-ways',
-            '--complex-ways',
-            '--out-pbf',
-            str(parent_file.absolute())
-        ]
-        subprocess.run(cmd, cwd=str(parent_file.parent), check=True)
-    else:
-        print(f'{tile} already exists and is current. skipping')
+        If the tile is smaller than the intended target size it is considered done and moved to the out_dir.
+        If not, additional jobs are scheduled to further break it down.
 
-    if target_file.stat().st_size < target_size:
-        print(f'{tile} has reached target size')
-        os.rename(str(target_file), str(out_dir / f'{tile[2]}_{tile[0]}_{tile[1]}.pbf'))
+        :param tile: Target tile which should be generated
+        """
 
+        box = mercantile.bounds(tile)
+        parent = mercantile.parent(tile)
 
-def extract(working_dir: Path, out_dir: Path, target_size: int):
-    print('Extracting tiles')
+        parent_file = self.working_dir / f'{parent[2]}_{parent[0]}_{parent[1]}.pbf'
+        target_file = self.working_dir / f'{tile[2]}_{tile[0]}_{tile[1]}.pbf'
 
-    subprocess.run(['ln', '-sf', (working_dir / 'source.osm.pbf').absolute(), (working_dir / '0_0_0.pbf').absolute()])
+        if not parent_file.exists():
+            print(f'Not generating {tile} because parent does not exist')
+            return
 
-    for z in range(1, 10):
-        with ThreadPool(processes=max(os.cpu_count() - 2, 1)) as pool:
-            for x in range(0, (2**z)):
-                for y in range(0, (2**z)):
-                    tile = mercantile.Tile(x, y, z)
-                    pool.apply_async(_process_tile, [working_dir, out_dir, target_size, tile])
-            pool.close()
-            pool.join()
+        if parent_file.stat().st_size < self.target_size:
+            print(f'Not generating {tile} because parent has reached target size')
+            return
+
+        if not target_file.exists() or parent_file.stat().st_mtime > target_file.stat().st_mtime:
+            # only build file if it does not exist
+            # or parent file has been modified since target was last generated
+            print(f'Generating {tile}')
+            cmd = [
+                'osmconvert',
+                f'-b={box.west},{box.south},{box.east},{box.north}',
+                f'-o={target_file.absolute()}',
+                '--complete-ways',
+                '--complex-ways',
+                '--out-pbf',
+                str(parent_file.absolute())
+            ]
+            subprocess.run(cmd, cwd=str(parent_file.parent), check=True)
+        else:
+            print(f'{tile} already exists and is current. skipping')
+
+        if target_file.stat().st_size < self.target_size:
+            print(f'{tile} has reached target size')
+            os.rename(str(target_file), str(self.out_dir / f'{tile[2]}_{tile[0]}_{tile[1]}.pbf'))
+        else:
+            self.extract(tile)
+
+    def _on_future_done(self, result: Future):
+        with self.lock_running_futures:
+            self.running_futures -= 1
+
+        # access result so that exceptions get properly logged
+        result.result()
+
+    def extract(self, source: mercantile.Tile):
+        """
+        Extract all sub-tiles from the source until the sub-tiles reach the target size or the maximum zoom level
+        has been reached.
+        """
+        z = source.z + 1
+        if z >= 10:
+            print(f'Asked to extract tiles from {source} but zoom level has reached maximum')
+            return
+
+        for x in range(source.x, source.x + 2):
+            for y in range(source.y, source.y + 2):
+                tile = mercantile.Tile(x, y, z)
+
+                future = self.executor.submit(self._generate_tile, tile)
+                with self.lock_running_futures:
+                    self.running_futures += 1
+                future.add_done_callback(lambda result: self._on_future_done(result))
 
 
 if __name__ == '__main__':
-    args = parse_args()
-    download_planet_dump(args.planet_dump, args.working_dir)
-    extract(args.working_dir, args.output_dir, args.target_size)
+    p = Program()
+    p.run()
