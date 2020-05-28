0ee5732deba2434decccbd607547a03b42e8e3c6
Finn-Thorben Sell <dev@finn-thorben.me>
2020-05-28 13:38:27 +0200

    do import in docker database

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,24 @@
 FROM docker.io/debian:buster-slim
 
 # install dependencies
+ARG PG_VERSION=11
+ENV PG_VERSION=${PG_VERSION}
 RUN apt update
-RUN apt install -y --no-install-recommends pipenv osmctools rsync
+RUN apt install -y --no-install-recommends pipenv python3-setuptools rsync postgresql-${PG_VERSION} \
+    osmctools osm2pgsql git libpq-dev gcc python3-dev
+RUN apt install -y postgresql-${PG_VERSION}-postgis
+
+RUN pg_dropcluster --stop ${PG_VERSION} main
 
 # add sources
 ADD Pipfile Pipfile.lock /app/src/
 WORKDIR /app/src
 RUN pipenv install --system --deploy --ignore-pipfile
-ADD osm_tile_data_extract /app/
+RUN apt purge -y libpq-dev gcc python3-dev
+RUN apt autoremove -y
+
+ADD main.py /app/src/
+ADD osm_tile_data_extract /app/src/osm_tile_data_extract
 
 # add image metadata
 VOLUME /app/tmp
--- osm_tile_data_extract/import_extracts.py
+++ osm_tile_data_extract/import_extracts.py
@@ -10,6 +10,9 @@ import mercantile
 from osm_tile_data_extract.util import *
 
 
+PG_VERSION = os.environ.get('PG_VERSION')
+
+
 class Program:
     @staticmethod
     def add_args(parser: argparse.ArgumentParser):
@@ -29,61 +32,52 @@ class Program:
         parser.add_argument('-z', type=int, required=True,
                             help='z coordinate of tile to import')
 
-        db_group = parser.add_argument_group(title='PostgreSQL connection parameters',
-                                             description='The configured user must be a superuser to create the needed'
-                                                         'PostgreSQL extensions (postgis and hstore).')
-        db_group.add_argument('--db-host', type=str, default='localhost')
-        db_group.add_argument('--db-port', type=int, default=5432)
-        db_group.add_argument('--db-user', type=str, default='osm_tile_data_extract')
-        db_group.add_argument('--db-password', type=str, default='osm_tile_data_extract')
-
     def __init__(self, args: argparse.Namespace):
         self.api = ApiClient(args.mapping_url, args.mapping_auth)
         self.tile = mercantile.Tile(args.x, args.y, args.z)
 
-        self.working_dir = args.working_dir
-        self.out_dir = args.output_dir
-
-        self.db_host = args.db_host
-        self.db_port = args.db_port
-        self.db_user = args.db_user
-        self.db_password = args.db_password
-        self.db_conn = None
+        self.working_dir = os.path.abspath(args.working_dir)
+        self.out_dir = os.path.abspath(args.output_dir)
+        self.pbf_file_name = None       # type: Union[None, str]
+        self.db_process = None          # type: Union[None, subprocess.Popen]
 
     def run(self):
-        print('Connecting to PostgreSQL server')
-        self.db_conn = psycopg2.connect(f'host={self.db_host} port={self.db_port} dbname=postgres '
-                                        f'user={self.db_user} password={self.db_password}')
-        self.db_conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-
         self._download_pbf()
         self._create_postgres_db()
-        self._import_into_postgres()
+        #self._import_into_postgres()
 
     def _download_pbf(self):
         print(f'Downloading pbf of {self.tile}')
         dump = self.api.get_planet_dump(self.tile)
         subprocess.run(['wget', '-N', '-nv', '--show-progress', dump['file']], check=True, cwd=self.working_dir)
+        self.pbf_file_name = str(dump['file']).rsplit('/')[-1]
 
     def _create_postgres_db(self):
-        database = f'osm_tile_data_extract_{self.tile.z}_{self.tile.x}_{self.tile.y}'
-        # create new database only if necessary
-        with self.db_conn.cursor() as cursor:
-            cursor.execute('SELECT datname FROM pg_database;')
-            if (database,) not in cursor.fetchall():
-                print(f'Creating PostgreSQL database {database}')
-                cursor.execute(f'CREATE DATABASE {database} OWNER %s', [self.db_user])
+        # create new postgresql cluster
+        db_dir = os.path.join(self.working_dir, 'pg_data')
+        subprocess.run(['rm', '-rf', db_dir])
+        os.makedirs(db_dir)
+        subprocess.run(['pg_createcluster', PG_VERSION, 'main', '--start', '--datadir', db_dir], check=True)
+        db_port = subprocess.run(['pg_lsclusters', '--no-header'], check=True, text=True, stdout=subprocess.PIPE)\
+            .stdout.split(' ')[2]
 
-        # reconnect to the correct database
-        self.db_conn.close()
-        self.db_conn = psycopg2.connect(f'host={self.db_host} port={self.db_port} dbname={database} '
-                                        f'user={self.db_user} password={self.db_password}')
-
-        # enable needed postgres extensions
-        print('Enabling needed PostgreSQL extensions')
-        with self.db_conn.cursor() as cursor:
-            cursor.execute('CREATE EXTENSION postgis;')
-            cursor.execute('CREATE EXTENSION hstore;')
+        # create database
+        print('Creating PostgreSQL database with extensions')
+        subprocess.run(['su', 'postgres', '-c', 'echo "CREATE USER tile_data PASSWORD \'tile_data\';" | psql'], check=True,
+                       stdout=subprocess.DEVNULL)
+        subprocess.run(['su', 'postgres', '-c', 'echo "CREATE DATABASE tile_data with OWNER tile_data;" | psql'],
+                       check=True, stdout=subprocess.DEVNULL)
+        subprocess.run(['su', 'postgres', '-c', 'echo "CREATE EXTENSION postgis;" | psql -d tile_data'],
+                       check=True, stdout=subprocess.DEVNULL)
+        subprocess.run(['su', 'postgres', '-c', 'echo "CREATE EXTENSION hstore;" | psql -d tile_data'],
+                       check=True, stdout=subprocess.DEVNULL)
 
     def _import_into_postgres(self):
-        pass
+        subprocess.run([
+            'osm2pgsql', '--slim', '--drop', '--hstore-all', '-C 3000', '-S osm2pgsql.style',
+            '-d', self.db_dbname,
+            '-P', self.db_port,
+            '-U', self.db_user,
+            '--number-processes', max(1, os.cpu_count() - 2),
+            os.path.join(self.working_dir, self.pbf_file_name)
+        ], check=True, cwd='')
